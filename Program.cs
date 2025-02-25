5f358eb76e74c7ed3dc7708ce4cd700152f8e222
363120981 <363120981@2260I-L29-004>
2025-02-25 14:45:42 -0500

    almost done.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,34 @@
-﻿using System.Diagnostics.Metrics;
+﻿using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Topic_8
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        
+            
+        public static void Main(string[] args)
         {
-            Random generator = new Random();
-
+            //run
             int run;
             int numOfParts = 2;
+
+            //part 1
+            string[] ColorsArray = new string[5];
+
+            //part 2
+            List<int> values = new List<int>();
+            int numberCount;
+            int minimum;
+            int maximum;
+            int search_number;
+            int search_number_found = 0;
+
+            Random generator = new Random();
+
+
             while (true) {
             {
                 Console.WriteLine("what part would you like to run?");
@@ -27,7 +46,6 @@ namespace Topic_8
                 {
                     //part 1
                     //here I used arrays (as they are more efficient), for part 2 I will use lists to show my understanding
-                    string[] ColorsArray = new string[5];
 
                         Console.WriteLine($"input {ColorsArray.Length} colors");
                     for (int i = 0; i < ColorsArray.Length; i++)
@@ -48,22 +66,74 @@ namespace Topic_8
                 else if (run == 2)
                 {
                     //part 2
-                    List<int> values = new List<int> ();
-                    int numberCount;
+                    
 
                     Console.WriteLine("How many numbers do you need?");
-                    while (Int32.TryParse(values.ToString(), out numberCount))
+                    while (!Int32.TryParse(Console.ReadLine(), out numberCount))
                         {
-                            values.Add(Console.ReadLine());
+                            Console.WriteLine("INVALID INPUT!");
+                        }
+
+                    Console.WriteLine("What is your maximum value?");
+                    while (!Int32.TryParse(Console.ReadLine(), out maximum))
+                        {
+                            Console.WriteLine("INVALID INPUT!");
+                        }
+
+                    Console.WriteLine("What is your minimum value?");
+                    while (!Int32.TryParse(Console.ReadLine(), out minimum))
+                        {
+                            Console.WriteLine("INVALID INPUT!");
+                        }
+
+                    //generate random numbers
+                    for (int i = 0; i < numberCount; i++)
+                        {
+                            values.Add(generator.Next(maximum-minimum+1) + minimum);
+                        }
+
+                    //print list in one line
+                    Console.WriteLine(string.Join(", ", values));
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Search for number.");
+                    while (!Int32.TryParse(Console.ReadLine(), out search_number))
+                        {
+                            Console.WriteLine("INVALID INPUT!");
                         }
 
                     for (int i = 0; i < values.Count; i++)
                         {
-
+                            if (values[i] == search_number)
+                            {
+                                search_number_found++;
+                            }
                         }
+                    Console.WriteLine($"your number was found, {search_number_found} times.");
 
+                    Console.WriteLine();
 
-                }
+                    Console.WriteLine("Search for number.");
+                    while (!Int32.TryParse(Console.ReadLine(), out search_number))
+                        {
+                            Console.WriteLine("INVALID INPUT!");
+                        }
+                    for (int i = 0; i < values.Count; i++)
+                        {
+                            if (values[i] == search_number)
+                            {
+                                values[i] = 0;
+                            }
+                        }
+                    Console.WriteLine(string.Join(", ", values));
+                    for (int i = 0; i < values.Count; i++)
+                        {
+                            values[i] = 0;
+                        }
+                        Console.WriteLine(string.Join(", ", values));
+
+                    }
 
                 }
                 Console.WriteLine();
